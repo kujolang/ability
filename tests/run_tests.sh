279f0b7a976136139c3fc4ed8e08e953ff9e86d9
Robert DeVore <deviodigital@gmail.com>
2026-09-01 17:11:06 -0400

    feat: add shared ability execution contracts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,3 +6,4 @@ KUJO_BIN="${KUJO_BIN:-$ABILITY_ROOT/../kujo/target/debug/kujo}"
 
 cd "$ABILITY_ROOT"
 "$KUJO_BIN" run tests/contract_tests.kujo --interpreter
+"$KUJO_BIN" run tests/runtime_contract_tests.kujo --interpreter
